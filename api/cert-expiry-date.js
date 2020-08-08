529c2e5a2117929f6fab9c4af97ecbb5fcf04d83
Xu Chunyang <xuchunyang56@gmail.com>
2020-08-08 19:13:47 +0800

    Add content-type and CORS HTTP header

--- api/cert-expiry-date.js
+++ api/cert-expiry-date.js
@@ -1,6 +1,9 @@
 const checkCertExpiration = require("check-cert-expiration");
 
 module.exports = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   const { url } = req.query;
   if (!url) {
     res.statusCode = 400;
